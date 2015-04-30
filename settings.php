a6e53c854e6493566a77d38c20d370b870692865
neswill1 <nenadkonatar@live.ac.me>
2015-04-30 10:50:32 +0200

    First commit

---
+++ settings.php
@@ -0,0 +1,23 @@
+<?php
+	$adresa[0]="1727 E Ohio Pike, Amelia, OH"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[0]="Amelia";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[0]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[0]="Amelia";
+	$telephone[0]=5137974038;// Broj koji se poziva na dugme CALL US
+
+	$adresa[1]="154 Main St., Batavia, OH"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[1]="Batavia";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[1]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[1]="Batavia";
+	$telephone[1]=5137326644;// Broj koji se poziva na dugme CALL US
+
+	$adresa[2]="120 E. Plane St., Bethel, OH"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[2]="Bethel";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[2]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[2]="Bethel";
+	$telephone[2]=5137343200;// Broj koji se poziva na dugme CALL US
+
+	$companynumber=3;
+	$theme="b";//Thema koja se aplicira na sve
+	$transitionefect="turn"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup", "turn", "slidefade", "flow"
+?>
